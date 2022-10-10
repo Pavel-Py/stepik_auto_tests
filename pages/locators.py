54014caec4ce984a4bd4fcddfb2ced59303bbc54
Pavel-Py <sollingen@gmail.com>
2022-10-10 15:58:45 +0300

    add test_product_page, product_page, locators

--- pages/locators.py
+++ pages/locators.py
@@ -8,3 +8,11 @@ class MainPageLocators:
 class LoginPageLocators:
     LOGIN_FORM = (By.CSS_SELECTOR, '#login_form')
     REGISTER_FORM = (By.CSS_SELECTOR, '#register_form')
+
+
+class ProductPageLocators:
+    BASKET_LINK = (By.CLASS_NAME, 'btn-add-to-basket')
+    BASKET_MESSAGE_PROD_NAME = (By.CSS_SELECTOR, '#messages>.alert:nth-child(1) strong')
+    PROD_NAME = (By.CSS_SELECTOR, '.product_main>h1')
+    PROD_PRICE = (By.CSS_SELECTOR, 'p.price_color')
+    BASKET_MESSAGE_PROD_PRICE = (By.CSS_SELECTOR, '.alertinner>p>strong')
---
+++ pages/product_page.py
@@ -0,0 +1,29 @@
+import time
+
+from .base_page import BasePage
+from .locators import ProductPageLocators
+
+
+class ProductPage(BasePage):
+    def add_product_to_basket(self):
+        add_basket_link = self.browser.find_element(*ProductPageLocators.BASKET_LINK)
+        add_basket_link.click()
+        self.solve_quiz_and_get_code()
+
+    def compare_prod_name_and_basket_message_name(self):
+        """
+        Сообщение о том, что товар добавлен в корзину.
+        Название товара в сообщении должно совпадать с тем товаром, который вы действительно добавили.
+        """
+        prod_name_in_alert = self.browser.find_element(*ProductPageLocators.BASKET_MESSAGE_PROD_NAME).text
+        prod_name = self.browser.find_element(*ProductPageLocators.PROD_NAME).text
+        assert prod_name_in_alert == prod_name
+
+    def compare_prod_price_and_basket_message_price(self):
+        """
+        Сообщение со стоимостью корзины. Стоимость корзины совпадает с ценой товара.
+        """
+        prod_price = self.browser.find_element(*ProductPageLocators.PROD_PRICE).text
+        prod_price_from_message = self.browser.find_element(*ProductPageLocators.BASKET_MESSAGE_PROD_PRICE).text
+        assert prod_price == prod_price_from_message
+
