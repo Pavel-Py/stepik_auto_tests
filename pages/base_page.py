89a8b660117c596f8c53e0e2d2a93bdba197e31f
Pavel-Py <sollingen@gmail.com>
2022-10-11 11:35:56 +0300

    moved go_to_login_page to base_page

--- pages/base_page.py
+++ pages/base_page.py
@@ -1,6 +1,10 @@
 import math
 
-from selenium.common.exceptions import NoSuchElementException, NoAlertPresentException
+from selenium.common.exceptions import NoSuchElementException, NoAlertPresentException, TimeoutException
+from selenium.webdriver.support import expected_conditions as ec
+from selenium.webdriver.support.wait import WebDriverWait
+
+from .locators import BasePageLocators
 
 
 class BasePage:
@@ -9,15 +13,37 @@ class BasePage:
         self.url = url
         self.browser.implicitly_wait(timeout)
 
-    def open(self):
-        self.browser.get(self.url)
-
-    def is_element_present(self, how, what):
+    def is_disappeared(self, method, selector, timeout=4):
         try:
-            self.browser.find_element(how, what)
+            WebDriverWait(self.browser, timeout, 1, [TimeoutException]). \
+                until_not(ec.presence_of_element_located((method, selector)))
+        except TimeoutException:
+            return False
+        return True
+
+    def is_element_present(self, method, selector):
+        try:
+            self.browser.find_element(method, selector)
         except NoSuchElementException:
             return False
         return True
+
+    def is_not_element_present(self, method, selector, timeout=4):
+        try:
+            WebDriverWait(self.browser, timeout).until(ec.presence_of_element_located((method, selector)))
+        except TimeoutException:
+            return True
+        return False
+
+    def go_to_login_page(self):
+        login_link = self.browser.find_element(*BasePageLocators.LOGIN_LINK)
+        login_link.click()
+
+    def open(self):
+        self.browser.get(self.url)
+
+    def should_be_login_link(self):
+        assert self.is_element_present(*BasePageLocators.LOGIN_LINK), "Login link is not presented"
 
     def solve_quiz_and_get_code(self):
         alert = self.browser.switch_to.alert
--- pages/product_page.py
+++ pages/product_page.py
@@ -1,11 +1,12 @@
-import time
-
 from .base_page import BasePage
 from .locators import ProductPageLocators
 
 
 class ProductPage(BasePage):
     def add_product_to_basket(self):
+        """
+        Добавляет товар в корзину.
+        """
         add_basket_link = self.browser.find_element(*ProductPageLocators.BASKET_LINK)
         add_basket_link.click()
         self.solve_quiz_and_get_code()
@@ -27,3 +28,12 @@ class ProductPage(BasePage):
         prod_price_from_message = self.browser.find_element(*ProductPageLocators.BASKET_MESSAGE_PROD_PRICE).text
         assert prod_price == prod_price_from_message
 
+    def should_not_be_success_message(self):
+        """
+        Проверяет отсутствие элемента на странице.
+        """
+        assert self.is_not_element_present(*ProductPageLocators.BASKET_MESSAGE_PROD_NAME), \
+            'Сообщение о добавлении в корзину появилось, хотя не должно было'
+
+    def should_disappeared_success_message(self):
+        assert self.is_disappeared(*ProductPageLocators.BASKET_MESSAGE_PROD_NAME)
